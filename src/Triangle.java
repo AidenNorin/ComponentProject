11856fa4412d72c5b99ff6e9a66c571e6315cc46
AidenNorin <aidennorin@gmail.com>
2026-03-08 23:51:04 -0400

    pivot to abstract implementation

--- src/Triangle.java
+++ src/Triangle.java
@@ -9,24 +9,6 @@ public interface Triangle extends TriangleKernel {
      *
      * @return The area of the triangle in {@code this}
      */
-    public double area() {
-
-        double triangleArea = 0.0;
-
-        double determinant = 0.0;
-
-        for (int i = 0; i < this.three; i++) {
-            determinant += (this.vertices[i]
-                    * this.vertices[i + this.dimensions + 1])
-                    - this.vertices[Math.floorMod(this.three - (i * 2),
-                            this.vertices.length)]
-                            * this.vertices[Math.floorMod(
-                                    this.three - (i * 2) + 1,
-                                    this.vertices.length)];
-        }
-
-        triangleArea = this.half * determinant;
-        return triangleArea;
-    }
+    double area();
 
 }
--- src/TriangleKernel.java
+++ src/TriangleKernel.java
@@ -1,5 +1,7 @@
+import components.standard.Standard;
+
 /**
- * TriangleKernel implementation
+ * TriangleKernel implementation.
  *
  * @author Aiden Norin
  */
@@ -8,18 +10,13 @@ public interface TriangleKernel extends Standard<Triangle> {
     /**
      * constant to suppress magic number waring that drives me crazy.
      */
-    private int three = 3;
+    int THREE = 3;
 
     /**
      * constant reused in many equations for triangles due to the area of a
      * parallelogram divided by 2 results in the area of the resulting triangle.
      */
-    private double half = 0.5;
-
-    /**
-     * The number of vertices defined in the triangle described by {@code this}
-     */
-    private int numVertices = 0;
+    double HALF = 0.5;
 
     // Kernel Methods
 
@@ -29,58 +26,31 @@ public interface TriangleKernel extends Standard<Triangle> {
      * @param coordinates
      *            coordinates of vertice to be added to {@code vertices}
      */
-    void addVertice(double[] coordinates) {
-        assert this.dimensions != coordinates.length
-                / this.three : "Violation of: dimensions of coordinates,"
-                        + "and this.dimensions are equal";
-        assert this.numVertices < this.three : "Violation of: triangle has"
-                + "less than 3 vertices";
-
-        for (int i = 0; i < this.dimensions; i++) {
-            this.vertices[i + (this.numVertices
-                    * this.dimensions)] = coordinates[i];
-        }
-        this.numVertices++;
-    }
+    void addVertice(double[] coordinates);
 
     /**
-     * Removes a vertice from {@code vertices}.
+     * Removes an arbitrary vertice from {@code vertices}.
      *
      * @return The removed vertice from {@code vertices}
      */
-    double[] removeAnyVertice() {
-        assert this.numVertices > 0 : "Violation of: triangle has no vertices";
-
-        this.numVertices--;
-
-        double[] vertice = new double[this.dimensions];
-        int index = 0;
-        for (int i = (this.numVertices - 1)
-                * this.dimensions; i < this.numVertices
-                        * this.dimensions; i++) {
-            vertice[index] = this.vertices[i];
-            index++;
-            this.vertices[i] = 0.0;
-        }
-        return vertice;
-    }
+    double[] removeAnyVertice();
 
     /**
-     * thingy.
-     */
-    double[] removeVertice() {
-        return 0.0;
-    }
-
-    /**
-     * Gives the number of vertices defined in the {@code this}
+     * removes a specific vertice from {@code vertices}.
      *
-     * @return
-     *          number of vertices in {@code this}
+     * @param vertice
+     *            integer corresponding to the vertice to be removed.
+     * @return the coordinates of the removed {@code vertice}
+     *
      */
-    int numVertices() {
-        return numVertices;
-    }
+    double[] removeVertice(int vertice);
+
+    /**
+     * The number of vertices defined in the {@code this}.
+     *
+     * @return number of vertices in {@code this}
+     */
+    int numVertices();
 
     /**
      * Returns the coordinates of the vertice corresponding to the given
@@ -97,15 +67,5 @@ public interface TriangleKernel extends Standard<Triangle> {
      *
      * @return The corresponding vertice to the {@code index}
      */
-    double[] getVertice(int index) {
-        double[] vertice = new double[this.dimensions];
-
-        int counter = 0;
-        for (int i = index * this.dimensions; i < index
-                * this.vertices.length; i++) {
-            vertice[counter] = this.vertices[i];
-            counter++;
-        }
-        return vertice;
-    }
+    double[] getVertice(int index);
 }
