7e5acb9633cdfe5f4121e466f7f72b26b6080490
AidenNorin <aidennorin@gmail.com>
2026-03-09 20:31:47 -0400

    Triangle Type

--- src/Triangle.java
+++ src/Triangle.java
@@ -11,4 +11,23 @@ public interface Triangle extends TriangleKernel {
      */
     double area();
 
+    /**
+     * Returns whether `this` is acute, obtuse, right, or equilateral in the
+     * form of a {@code String}.
+     *
+     * @requires No two {@code vertice} in {@code this} should have the same
+     *           coordinates.
+     *
+     * @ensure triangleType = "acute" if {@code this} is an acute triangle.
+     *         triangleType = "obtuse" if {@code this} an obtuse triangle.
+     *         triangleType = "right" if {@code this} is a right triangle.
+     *         triangleType = "equilateral" if {@code this} is an equilateral
+     *         triangle.
+     *
+     * @return {@code String} corresponding to the type of triangle that
+     *         {@code this} is.
+     */
+    String triangleType();
+
+    
 }
